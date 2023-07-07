07e9bc99e9732e5daf6091e082d8026279d190d3
Thiago <thiago.kaique@stayfilm.com.br>
2023-07-07 11:19:42 -0300

    added modal

---
+++ components/modal.jsx
@@ -0,0 +1,34 @@
+import './assets/modal.css'
+var zindex = 1000;
+
+export { Modal }
+
+function Modal(content,onClose)
+{
+    const close_button = ref()
+    const prompt = ref()
+    const modal = 
+    <div class="modal-background" v-show="visible">
+        <div class="modal-prompt" ref={prompt}>
+            <span class="close-button" ref={close_button} id="close">✖</span>{content}</div>
+    </div>
+
+    prompt.$on("click",e => e.stopPropagation())
+
+
+    modal.$on("click",(e)=>{
+        if(prompt.contains(e.target)) return
+        close()
+    })
+
+    modal.$style({"z-index":zindex++})
+    function close()
+    {
+        if(onClose) onClose()
+        zindex--
+        modal.remove()
+    }
+    close_button.$on("click",close)
+    modal.$parent(document.body)
+    return {close}
+}
