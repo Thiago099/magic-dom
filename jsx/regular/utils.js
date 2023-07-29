333aff181bb57b1e4953a80be29d70b8632a7ebe
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2023-07-29 14:10:24 -0300

    fix

--- jsx/regular/utils.js
+++ jsx/regular/utils.js
@@ -29,7 +29,7 @@ function dig(element)
     const properties = []
     while(typeof element == "object" && element.$key == "471ddd10-6cc3-429b-ba9a-5f4250686d4a")
     {
-        properties.push(...element.properties)
+        properties.push(...element.properties.filter(x=>x != undefined))
         element = element.action()
     }
     return {element, properties}
