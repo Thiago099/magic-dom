8680a4d1a4895b3dd02fb47352ff73c2ff9a58c8
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2023-08-13 18:06:55 -0300

    version

--- parser/scopedCss.js
+++ parser/scopedCss.js
@@ -7,17 +7,26 @@ function transform(code,id)
 
     const matchRule = "[data-css-scope-jdbe=\""+id.substring(0,id.length-7).replace(/[^A-Za-z0-9]+/g,"-")+"\"]";
 
-    for(const rule of sheet)
+    loop(sheet)
+    function loop(sheet)
     {
-        for(const index in rule.selectors)
+        for(const rule of sheet)
         {
-            if(rule.selectors[index].includes(":root"))
+            if(rule.type != "rule" && rule.rules != null)
             {
-                rule.selectors[index] = rule.selectors[index].replace(/:root/g, matchRule)
+                loop(rule.rules)
+                continue
             }
-            else
+            for(const index in rule.selectors)
             {
-                rule.selectors[index] = matchRule+" "+rule.selectors[index]
+                if(rule.selectors[index].includes(":root"))
+                {
+                    rule.selectors[index] = rule.selectors[index].replace(/:root/g, matchRule)
+                }
+                else
+                {
+                    rule.selectors[index] = matchRule+" "+rule.selectors[index]
+                }
             }
         }
     }
